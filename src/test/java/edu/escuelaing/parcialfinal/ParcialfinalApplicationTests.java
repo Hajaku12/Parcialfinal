3424d33eb1cadca16f4fd4281f21ffa7fd5e6c10
Hann Jang <hann.jang@mail.escuelaing.edu.co>
2024-07-02 09:01:15 -0500

    First commit

---
+++ src/test/java/edu/escuelaing/parcialfinal/ParcialfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.parcialfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
